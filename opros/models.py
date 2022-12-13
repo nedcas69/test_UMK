cd8b80357ff9cc5ebf64e506681835cf477761e1
nedcas69 <nedcas69@github.com>
2022-12-13 11:31:44 +0500

    fff

--- opros/models.py
+++ opros/models.py
@@ -61,7 +61,7 @@ class Quiz(models.Model):
 class UserInput(models.Model):
     user_name = CharField('ФИО', max_length=250,)
     random_list = CharField('random list', max_length=250, blank=True)
-    user_number = IntegerField('Табельный номер', blank=True)
+    user_number = IntegerField('Табельный номер', blank=True, null=True)
     result = BooleanField('Результат', default=False)
 
     def __str__(self):
